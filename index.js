fdd2928351105fe83f9f46275792e2068ece41d4
levi <levihalperin1998@gmail.com>
2021-12-07 16:19:32 +0000

    search redfin for Arr of addresses

--- index.js
+++ index.js
@@ -1,30 +1,47 @@
 const puppeteer = require("puppeteer");
 
-(async () => {
-    let address = "3737 Meramec St"
-    const browser = await puppeteer.launch({
-        headless: false,
-        defaultViewport: false
+async function searchRedfin(addresses) {
+
+    addresses.forEach(async (address) => {
+        const browser = await puppeteer.launch({
+            headless: true,
+            defaultViewport: false
+        })
+
+        const page = await browser.newPage()
+        await page.goto("https://www.redfin.com/")
+
+        await page.type("#search-box-input", address)
+        await page.click("#tabContentId0 > div > div > form > div > button")
+        await page.waitForNavigation();
+
+        let agentsName = await page.evaluate(() => {
+            let el = document.querySelector("#sideBarContent > div > div > div.primaryBody > div.padding-top-medium > div.font-weight-bold")
+            return el ? el.innerText : ""
+        })
+        let brokerage = await page.evaluate(() => {
+            let el = document.querySelector("#sideBarContent > div > div > div.primaryBody > div.padding-top-medium > div:nth-child(2)")
+            return el ? el.innerText : ""
+        })
+        let phone = await page.evaluate(() => {
+            let el = document.querySelector("#sideBarContent > div > div > div.primaryBody > div.padding-top-medium > div:nth-child(3) > a")
+            return el ? el.innerText : ""
+        })
+
+        let email = await page.evaluate(() => {
+            let el = document.querySelector("#sideBarContent > div > div > div.primaryBody > div.padding-top-medium > div:nth-child(4) > a")
+            return el ? el.innerText : ""
+        })
+
+        browser.close()
+
+        // console.log(`|Address : ${address}|`, `|Name : ${agentsName}| `, `|Brokerage : ${brokerage}|`, `|Phone : ${phone}|`, `|Email : ${email}|`)
+
+
+        
     })
+}
 
 
-    const page = await browser.newPage()
-    await page.goto("https://www.redfin.com/")
+console.log(searchRedfin(["3737 Meramec St st louis", "3649 Koeln Ave st louis", "4053 Connecticut st louis"]))
 
-    await page.type("#search-box-input", address, {
-        delay: 100
-    })
-    await page.click("#tabContentId0 > div > div > form > div > button")
-    await page.waitForNavigation();
-
-
-    const name = await page.$eval("#sideBarContent > div > div > div.primaryBody > div.padding-top-medium > div.font-weight-bold", el => el.textContent)
-    const brokerage = await page.$eval("#sideBarContent > div > div > div.primaryBody > div.padding-top-medium > div:nth-child(2)", el => el.textContent)
-    const phone = await page.$eval("#sideBarContent > div > div > div.primaryBody > div.padding-top-medium > div:nth-child(3) > a", el => el.textContent)
-    const email = await page.$eval("#sideBarContent > div > div > div.primaryBody > div.padding-top-medium > div:nth-child(4) > a", el => el.textContent)
-    console.log(`name:${name}, brokerage:${brokerage} Phone Number:${phone} email:${email}`)
-
-
-
-
-})()
